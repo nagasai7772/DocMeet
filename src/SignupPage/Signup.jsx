19614245b63f580e1b70118d921e5a551ab30379
nagasai7772 <nagunagasai@gmail.com>
2025-04-25 15:35:40 +0530

    changes

--- src/SignupPage/Signup.jsx
+++
@@ -1,9 +0,0 @@
-import SignupImage from '../Images/SignupImage'; // Ensure this is the correct path
-
-export const Signup = () => {
-  return (
-    <div className="">
-      <img src={SignupImage} alt="Signup" />
-    </div>
-  );
-};
